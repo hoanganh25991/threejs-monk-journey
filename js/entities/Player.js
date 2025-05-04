661f8448702bbb5170fad30f557615eef3471daa
Anh Le <lehoanganh25991@gmail.com>
2025-05-04 10:26:27 +0700

    chore: auto commit 20250504-102627

--- js/entities/Player.js
+++ js/entities/Player.js
@@ -172,7 +172,7 @@ export class Player {
                 type: 'ranged',
                 damage: 20,
                 manaCost: 15,
-                cooldown: 1, // Reduced cooldown
+                cooldown: 0.5, // Reduced cooldown
                 range: 10,
                 radius: 2,
                 duration: 3.5, // Further increased duration from 2.5 to 3.5
@@ -184,7 +184,7 @@ export class Player {
                 type: 'aoe',
                 damage: 15,
                 manaCost: 25,
-                cooldown: 1, // Reduced cooldown
+                cooldown: 0.5, // Reduced cooldown
                 range: 5,
                 radius: 4,
                 duration: 2.5, // Further increased duration from 1.5 to 2.5
@@ -196,7 +196,7 @@ export class Player {
                 type: 'multi',
                 damage: 10,
                 manaCost: 30,
-                cooldown: 1, // Reduced cooldown
+                cooldown: 0.5, // Reduced cooldown
                 range: 6,
                 radius: 10,
                 duration: 5.0, // Further increased duration from 3.5 to 5.0
@@ -209,7 +209,7 @@ export class Player {
                 type: 'buff',
                 damage: 10,
                 manaCost: 20,
-                cooldown: 1, // Reduced cooldown
+                cooldown: 0.5, // Reduced cooldown
                 range: 0,
                 radius: 5,
                 duration: 10, // Further increased duration from 7 to 10
@@ -221,7 +221,7 @@ export class Player {
                 type: 'summon',
                 damage: 8,
                 manaCost: 35,
-                cooldown: 1, // Reduced cooldown
+                cooldown: 0.5, // Reduced cooldown
                 range: 2,
                 radius: 1,
                 duration: 20, // Further increased duration from 15 to 20
@@ -233,7 +233,7 @@ export class Player {
                 type: 'wave',
                 damage: 50,
                 manaCost: 40,
-                cooldown: 1, // Reduced cooldown
+                cooldown: 0.5, // Reduced cooldown
                 range: 10,
                 radius: 5,
                 duration: 5.0, // Further increased duration from 3.5 to 5.0
@@ -245,7 +245,7 @@ export class Player {
                 type: 'mark',
                 damage: 15,
                 manaCost: 25,
-                cooldown: 1, // Reduced cooldown
+                cooldown: 0.5, // Reduced cooldown
                 range: 10,
                 radius: 5,
                 duration: 20, // Further increased duration from 15 to 20 seconds
