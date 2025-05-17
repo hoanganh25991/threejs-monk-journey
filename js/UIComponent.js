324609e9985f6d1a6056516e0be27a69735ec7c5
Anh Le <lehoanganh25991@gmail.com>
2025-05-17 14:33:48 +0700

    chore: auto commit 20250517-143348

--- js/UIComponent.js
+++ js/UIComponent.js
@@ -20,12 +20,7 @@ export class UIComponent {
             this.container.id = containerId;
             
             // Add to UI container
-            const uiContainer = document.getElementById('hud-container');
-            if (uiContainer) {
-                uiContainer.appendChild(this.container);
-            } else {
-                document.body.appendChild(this.container);
-            }
+            document.body.appendChild(this.container);
         }
     }
     
